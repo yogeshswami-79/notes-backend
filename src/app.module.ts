70eebeaae6f0e10a19ee1ae2c3ce6fa90ef09c0b
aluparsad <aluparsad79@gmail.com>
2022-12-07 17:32:02 +0530

    backend notes

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,30 @@ import { AuthModule } from './auth/auth.module';
 import { NotesModule } from './notes/notes.module';
 
 
+const man = {
+  type: "postgres",
+  host: "localhost",
+  port: 9799,
+  username: "icmi",
+  password: "password",
+  database: "notes",
+  logging: true,
+  entities: ['dist/**/*.entity.{js,ts}'],
+}
+
 @Module({
 
   imports: [
-    TypeOrmModule.forRoot( {
+    TypeOrmModule.forRoot({
       type: "postgres",
-      host: "localhost",
-      port: 9799,
+      host: "postgresql-99214-0.cloudclusters.net",
+      port: 17448,
       username: "icmi",
       password: "password",
       database: "notes",
       logging: true,
       entities: ['dist/**/*.entity.{js,ts}'],
-    } ),
+    }),
 
     AuthModule,
     NotesModule
